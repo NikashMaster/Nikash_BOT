f40f79ee3f49759d1bcd3182018321771f5cd01e
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-06-26 08:51:29 +0530

    Update rmarval.js

--- plugins/rmarval.js
+++ plugins/rmarval.js
@@ -1,40 +1,42 @@
-/* # Exclusively from Msjessica 
-# script original credit gos to @lasiya99X t.me/lasiya99X ©danuma project 
-# Do not use this fore any commercial thing 
+/*  
+# Do not use this fore any commercial thing
 # If you abuse thais bot we wil kick you from bot 
-# Copyright 2021 All rights reserved 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
 */
 
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
+const request = require('request');
+const got = require("got");
 const Config = require('../config');
-const need = "*type some word after command*\n*විධානයට පසුව වචනයක් ලියන්න"
+const des = "You Can Png From Any Emoji"
+const iii = "type only emoji"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'rmarvel ?(.*)', fromMe: true }, (async (message, match) => {
+    Asena.addCommand({pattern: 'rmarval ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(need);
+        if (match[1] === '') return await message.sendMessage(iii);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'RAVANA💢' })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by RAVANA'})
 
     }));
 }
-
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'rmarvel ?(.*)', fromMe: true }, (async (message, match) => {
+    Asena.addCommand({pattern: 'rmarval ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(need);
+        if (match[1] === '') return await message.sendMessage(iii);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'RAVANA💢' })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by RAVANA.'})
 
     }));
-    
 }
