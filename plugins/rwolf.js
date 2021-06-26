f4e5830675f8f94e6c88ace1d90c2a0d6645dace
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-06-26 09:18:27 +0530

    Update rwolf.js

--- plugins/rwolf.js
+++ plugins/rwolf.js
@@ -1,42 +1,37 @@
-/* 
-# Do not use this fore any commercial thing
-# If you abuse thais bot we wil kick you from bot 
-# Do not edit (Respect to the Devaoloper) 
-# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
-# Get more about devaoloper https://lasiya.ml
+
+/* # Exclusively from RAVANA
 */
 
 const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
-const request = require('request');
-const got = require("got");
 const Config = require('../config');
-const des = "You Can Png From Any Emoji"
-const iii = "type only emoji"
+const need = "type some word after command\n🗡🗡"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'rwolf ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rwolf ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(iii);
+        if (match[1] === '') return await message.sendMessage(need);
 
-        var webimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA&text2=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by RAVANA'})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Msjessica*' })
 
     }));
 }
+
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'rwolf ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rwolf ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(iii);
+        if (match[1] === '') return await message.sendMessage(need);
 
-        var webimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA&text2=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by RAVANA.'})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Msjessica*' })
 
     }));
+    
 }
