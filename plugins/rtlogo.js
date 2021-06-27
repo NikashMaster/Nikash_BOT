9d207875d8d7a8520983239ec0bef1049d477119
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-06-27 19:38:46 +0530

    Update and rename rlogo.js to rtlogo.js

--- plugins/rtlogo.js
+++ plugins/rtlogo.js
@@ -12,7 +12,7 @@ const lol = "need some word\n🗡🗡"
 
     if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'rlogo ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rtlogo ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
@@ -25,7 +25,7 @@ const lol = "need some word\n🗡🗡"
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'rlogo ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rtlogo ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
