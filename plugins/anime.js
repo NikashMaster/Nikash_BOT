163a5d119499acb34f4ffa4ec5f24bdb88dad87e
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-06-27 07:59:43 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -8,7 +8,7 @@ const Lang = Language.getString('anime');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: true, desc: Lang.TR}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: true, desc: Lang.AN}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -102,7 +102,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: false, desc: Lang.TR}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: false, desc: Lang.AN}, (async (message, match) => {
 
     var r_text = new Array ();
 
