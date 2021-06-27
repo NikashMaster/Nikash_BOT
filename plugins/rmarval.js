cea554c16e7f7663bf0a223c820afaedd914260e
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-06-27 23:09:05 +0530

    Update rmarval.js

--- plugins/rmarval.js
+++ plugins/rmarval.js
@@ -1,4 +1,4 @@
-/* # Exclusively from RAVANA 
+/* # Exclusively from Msjessica 
 # script original credit gos to @lasiya99X t.me/lasiya99X ©danuma project 
 # Do not use this fore any commercial thing 
 # If you abuse thais bot we wil kick you from bot 
@@ -20,7 +20,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by  RAVANA' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '💫Made by Ms RAVANA' })
 
     }));
 }
@@ -33,7 +33,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: ' Made by  RAVANA' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '💫Made by Ms RAVANA' })
 
     }));
     
