10a5dbaefb6cb9ad8ce6acdacdb547f4ce12fc3b
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-03 10:11:17 +0530

    Update rmarval.js

--- plugins/rmarval.js
+++ plugins/rmarval.js
@@ -1,8 +1,4 @@
-/* # Exclusively from Msjessica 
-# script original credit gos to @lasiya99X t.me/lasiya99X ©danuma project 
-# Do not use this fore any commercial thing 
-# If you abuse thais bot we wil kick you from bot 
-# Copyright 2021 All rights reserved 
+/* # Exclusively from RAVANA
 */
 
 const Asena = require('../events');
@@ -10,30 +6,30 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "*type some word after command*\n*විධානයට පසුව වචනයක් ලියන්න"
+const need = "type some word after command\n🗡🗡"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'rmarvel ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rmarval ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '💫Made by Ms RAVANA' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
 
     }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'rmarvel ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'rmarval ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/marvellogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=${encodeURIComponent(match[1])}&text2=STUDIO`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '💫Made by Ms RAVANA' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
 
     }));
     
