7a7fcc99dac9314907bb53a870eac204a6f95a27
SLBADBOY <83726709+SLBADBOY@users.noreply.github.com>
2021-07-05 18:09:12 +0530

    Create mfire.js

---
+++ plugins/mfire.js
@@ -0,0 +1,38 @@
+  
+  
+/* # Exclusively from 💕 Maraya
+*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "type some word after command\n🗡🗡"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'mfire ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/burning-image?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&image=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 💕 Maraya*' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'mfire ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/burning-image?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&image=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 💕 Maraya*' })
+
+    }));
+    
+}
