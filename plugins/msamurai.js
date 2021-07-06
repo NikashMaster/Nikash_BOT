84f14cbbd3fec810dfa26dca3f7359c82c2df30d
SLBADBOY <83726709+SLBADBOY@users.noreply.github.com>
2021-07-06 08:25:20 +0530

    Create msamurai.js

---
+++ plugins/msamurai.js
@@ -0,0 +1,38 @@
+  
+  
+/* # Exclusively from 💕 Maraya
+*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "type some word after command\n🗡🗡"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'msamurai ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=d60d072e68c6de0fff0c499d&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 💕 Maraya*' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'msamurai ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=d60d072e68c6de0fff0c499d&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 💕 Maraya*' })
+
+    }));
+    
+}
