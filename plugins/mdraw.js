fc1fabd15defa7e56d33784fdaf7f5ecd368af3b
SLBADBOY <83726709+SLBADBOY@users.noreply.github.com>
2021-07-06 16:55:18 +0530

    Update mdraw.js

--- plugins/mdraw.js
+++ plugins/mdraw.js
@@ -15,6 +15,8 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({ pattern: 'mdraw ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
+              if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/draw-image?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&image=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
@@ -28,10 +30,10 @@ else if (Config.WORKTYPE == 'public') {
     Asena.addCommand({ pattern: 'mdraw ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
+              if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/draw-image?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&image=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
-        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 💕 Maraya*' })
 
     }));
