19c1a28ca5e0b22f35e3b2b536c70341688d5d0d
NikashMaster <87128818+NikashMaster@users.noreply.github.com>
2021-09-12 14:30:37 +0530

    Update SPAMCENTER.js

--- plugins/SPAMCENTER.js
+++ plugins/SPAMCENTER.js
@@ -9,3 +9,9 @@ const SPAM = "Bot SPAM Center"
     
     }));
     
+ MARAYA.addCommand({pattern: 'mspam', fromMe: false, deleteCommand: true, desc: SPAM,}, (async (message, match) => {
+
+        await message.sendMessage("*WELLCOME MARAYA SPAM CENTER* \n\n✨BEST SPAMS\n\n\n▶(.1) BANG CORONA SPAM\n\n▶(.2) INSTAGRAM LINK SPAM\n\n▶(.3) CHINA SPAM\n\n▶(.4) 𝑴𝑨𝑿老༅࿐老༅࿐ SPAM\n\n\n\n MARAYA SPAM CENTER  භාවිතා කිරීම ගැන ඔබට ස්තුතියි");
+    
+    }));
+    
