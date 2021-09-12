fc5384861650ad046d4f9310ce422bd8b6b8d384
NikashMaster <87128818+NikashMaster@users.noreply.github.com>
2021-09-12 14:24:16 +0530

    Add files via upload

---
+++ plugins/SPAMCENTER.js
@@ -0,0 +1,11 @@
+const MARAYA  = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const SPAM = "Bot SPAM Center"
+
+
+    MARAYA.addCommand({pattern: 'mspam', fromMe: true, deleteCommand: true, desc: SPAM,}, (async (message, match) => {
+
+        await message.sendMessage("*WELLCOME MARAYA SPAM CENTER* \n\n✨BEST SPAMS\n\n\n▶(.1) BANG CORONA SPAM\n\n▶(.2) INSTAGRAM LINK SPAM\n\n▶(.3) CHINA SPAM\n\n▶(.4) 𝑴𝑨𝑿老༅࿐老༅࿐ SPAM\n\n\n\n MARAYA SPAM CENTER  භාවිතා කිරීම ගැන ඔබට ස්තුතියි");
+    
+    }));
+    
